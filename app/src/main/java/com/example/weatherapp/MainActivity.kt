3b513cb4de33fd3c8892f1a9d4371b3e48c28c0c
Abinash <abinashdash6000@gmail.com>
2023-06-04 04:30:11 +0530

    Made the UI responsive by making api call in defferent thread(coroutine scope)

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.view.WindowCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +18,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
         val viewModel: WeatherViewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
         if(viewModel.result.value!=null)
             runBlocking {
@@ -26,8 +28,8 @@ class MainActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         binding.textInputLayout.setEndIconOnClickListener {
             viewModel.setCityValue(binding.textInputEditText.text.toString())
-            viewModel.loadWeatherData()
-            runBlocking {
+            MainScope().launch {
+                viewModel.loadWeatherData()
                 binding.recyclerView.adapter = ItemAdapter(this@MainActivity, viewModel.result.value!!.await().forecast?.forecastday)
             }
         }
--- app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
@@ -3,6 +3,7 @@ package com.example.weatherapp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.weatherapp.network.Weather
 import com.example.weatherapp.network.WeatherApi
 import kotlinx.coroutines.*
@@ -19,11 +20,10 @@ class WeatherViewModel : ViewModel() {
     fun setCityValue(text: String) {
         _city.value = text
     }
-
-    fun loadWeatherData() {
-        runBlocking {
+    suspend fun loadWeatherData() {
+        viewModelScope.launch {
             try {
-                _result.value = async(Dispatchers.IO) { WeatherApi.retrofitService.getWeather(city.value.toString()) } // async is doing the api call asynchronously
+                _result.value = async { WeatherApi.retrofitService.getWeather(city.value.toString()) } // async is doing the api call asynchronously
                 _temperature.value = result.value!!.await().current?.tempC
             } catch (e: Exception) {
                 _city.value = e.toString()
