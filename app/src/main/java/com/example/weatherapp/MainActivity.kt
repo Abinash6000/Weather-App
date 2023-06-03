d2b91ef553a21885a0780145b94a3b773941fa14
Abinash <abinashdash6000@gmail.com>
2023-06-04 02:44:02 +0530

    second commit

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,28 +2,25 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.core.view.WindowCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.databinding.ActivityMainBinding
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         val viewModel: WeatherViewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
-
+        if(viewModel.result.value!=null)
+            runBlocking {
+                binding.recyclerView.adapter = ItemAdapter(this@MainActivity, viewModel.result.value!!.await().forecast?.forecastday)
+            }
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
